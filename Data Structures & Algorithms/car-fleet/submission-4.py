67654a1aa16fa597a00539fde3927e0fd7c6175f
Santosh Dodhi <143887575+Santosh-Dodhi@users.noreply.github.com>
2026-04-08 22:05:35 +0530

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        """
+        time: O(nlogn) sorting
+        space: O(n) zip for sorting
+        approach: (no stack) track last_time
+        """
+        n = len(position)
+        # sort based on position with decreasing
+        cars = sorted(zip(position, speed), reverse=True)
+        ans = 0
+        last_ele = -1
+        for pos, spd in cars:
+            dist = target - pos
+            time = dist/spd
+            if last_ele < time:
+                ans += 1
+                last_ele = time
+        return ans
+
+        
