79bffa444e37e894867275919f6a6a69b122fda0
Santosh Dodhi <143887575+Santosh-Dodhi@users.noreply.github.com>
2026-04-13 22:10:25 +0530

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.py
@@ -0,0 +1,51 @@
+class TimeMap:
+    from collections import defaultdict
+    def __init__(self):
+        self.dic = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.dic[key].append((timestamp, value))
+        
+    def get(self, key: str, timestamp: int) -> str:
+        # # print(self.dic)
+        # nums = self.dic[key]
+        # # print(nums)
+        # if not nums:
+        #     return ""
+        # n = len(nums)
+        # i = n - 1
+        # while i >= 0:
+        #     if nums[i][0] == timestamp:
+        #         return nums[i][1]
+        #     elif nums[i][0] > timestamp:
+        #         i -= 1
+        #     else:
+        #         return nums[i][1]
+        # return ""
+
+        """lower bound binary search"""
+        nums = self.dic[key]
+        if not nums:
+            return ""
+        n = len(nums)
+        st, end = 0, n-1
+        while st <= end:
+            mid = st + (end-st)//2
+            if nums[mid][0] >= timestamp:
+                end = mid - 1
+            else:
+                st = mid + 1
+        if st < n and nums[st][0] == timestamp:
+            return nums[st][1]
+        elif end >= 0:
+            return nums[end][1]
+        else: 
+            return ""
+            
+
+            
+            
+            
+
+
+        
