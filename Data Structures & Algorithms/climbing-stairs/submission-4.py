344da9616b28c57e8f3e73ecb4669e1734a3e671
Santosh Dodhi <143887575+Santosh-Dodhi@users.noreply.github.com>
2026-07-02 23:11:06 +0530

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,23 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [-1]*(n+1)
+
+        # def recursive(x):
+        #     if dp[x] != -1:
+        #         return dp[x]
+        #     if x in [1,2]: 
+        #         return x
+        #     dp[x] = recursive(x-1) + recursive(x-2)
+        #     return dp[x]
+
+        # return recursive(n)
+        if n in [1, 2]:
+            return n
+            
+        dp[1], dp[2] = 1, 2
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+
+
+        
